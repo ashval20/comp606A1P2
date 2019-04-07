7b9a0eeca0a2feff8000495bfe5d923e99e13e06
ashval20 <ashlyvalsalan1996@gmail.com>
2019-04-07 13:00:40 +1200

    homepage modified

---
+++ contactus.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Contact us</title>
+  	<link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <h1>Contact us for more details</h1>
+    <h2>Email us: bayly.m@hotmail.co.nz<br> ashlyvalsalan1996@gmail.com </h2>
+
+  </body>
+</html>
